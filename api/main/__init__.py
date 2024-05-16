8ce04c7ad019d6ecc152144683db67a97526470b
Compi-Craft <ozarko.pn@ucu.edu.ua>
2024-05-16 05:15:08 +0300

    api works!!!

--- api/main/__init__.py
+++ api/main/__init__.py
@@ -4,9 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from main.models import Base
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "HW3_SUPER_SECRET_KEY"
+CORS(app)
 engine = create_engine('sqlite:///petro_united.db', echo=False, pool_size = 0, max_overflow = -1)
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
--- app/src/App.js
+++ app/src/App.js
@@ -3,14 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home.jsx"
 import About from "./pages/about.jsx"
 import TeamSection from "./pages/team.js";
-
+import Test from "./components/test.jsx"
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/about" element={<About />} />
-        <Route path="/players" element={<TeamSection />} />
+      <Route path="/players" element={<TeamSection />} />
+      <Route path="/test" element={<Test />}/>
     </Routes>
   );
 }
