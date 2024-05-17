6ae4e58c4cf6bc5cbd8f6091f326635217e590a6
Compi-Craft <ozarko.pn@ucu.edu.ua>
2024-05-17 05:44:08 +0300

    video data

--- app/src/components/main.jsx
+++ app/src/components/main.jsx
@@ -2,26 +2,50 @@ import '../styles.css';
 import background from '../with_fans.jpg'
 import logo from '../logo.png'
 import enemy from '../enemy.png'
+import React, { useState, useEffect } from 'react';
 
 const Main = () => {
+    const [jsonData, setData] = useState(null);
+    useEffect(() => {
+      fetchData();
+    }, []);
+  
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/api/get_recent_highlights');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        console.log(jsonData);
+        console.log("GAY")
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
     return (
         <main className="main">
+            {jsonData ? (
+                <div>
             <div className="header-image">
                 <img src={background} alt="Heading Image" />
                 <h1>Petro United with Fans!</h1>
             </div>
 
             <div className="videos">
-                <h3>Videos by Petro United</h3>
-                {/* Videos content */}
-                <div className="content">
-                    {/* Individual video components */}
-                    {Array.from({ length: 6 }, (_, i) => (
-                        <div key={i} className="video">Subtitle</div>
-                    ))}
-                </div>
+                {/* Individual video components */}
+                {jsonData.map(video => (
+                    <div key={video.highlight_id} className="content">
+                        <h4>{video.title}</h4>
+                        <p>Date: {video.date}</p>
+                        <video controls className='video'>
+                            <source src={"http://localhost:5000/" + video.video} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                ))}
             </div>
-            {/* Last Match component */}
             <div className="last-match">
                 <div className="controls">
                     <h3>Last match review</h3>
@@ -43,6 +67,10 @@ const Main = () => {
                     </div>
                 </div>
             </div>
+            </div>
+            ) : (
+                <p>Loading...</p>
+            )};
         </main>
     );
 };
